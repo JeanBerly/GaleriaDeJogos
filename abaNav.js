278643efbc8ea30560db1e05da2698e4c22e7ec8
JeanBerly <jeanberlyy@gmail.com>
2022-01-19 12:03:45 -0300

    Adicionado os reviews de NFS e Portal

--- abaNav.js
+++ abaNav.js
@@ -1,6 +1,8 @@
 let navAberto = false;
+console.log('oi');
 function abrirNav(){
     let nav = document.getElementById('navPrincipal');
-    navAberto ? nav.style.zIndex = '-1' : nav.style.zIndex = '1';
+    navAberto ? nav.style.zIndex = '-1' : nav.style.zIndex = '999';
     navAberto = !navAberto;
+    console.log('oi dentro do abrir nav');
 }
